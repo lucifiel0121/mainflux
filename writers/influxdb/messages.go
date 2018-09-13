62c51056edc20451d8c8dffb72d74c53af5a01c5
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2018-09-13 19:08:17 +0200

    Add batch of streams
    Signed-off-by: Dušan Borovčanin <dusan.borovcanin@mainflux.com>

--- writers/influxdb/messages.go
+++ writers/influxdb/messages.go
@@ -9,6 +9,7 @@ package influxdb
 
 import (
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/mainflux/mainflux/writers"
@@ -22,34 +23,68 @@ const pointName = "messages"
 var _ writers.MessageRepository = (*influxRepo)(nil)
 
 type influxRepo struct {
-	database string
-	client   influxdata.Client
+	client    influxdata.Client
+	batch     []*influxdata.Point
+	batchSize int
+	mu        sync.Mutex
+	tick      <-chan time.Time
+	cfg       influxdata.BatchPointsConfig
 }
 
 type fields map[string]interface{}
 type tags map[string]string
 
 // New returns new InfluxDB writer.
-func New(client influxdata.Client, database string) (writers.MessageRepository, error) {
-	return &influxRepo{database, client}, nil
+func New(client influxdata.Client, database string, batchSize int, batchTimeout time.Duration) (writers.MessageRepository, error) {
+	repo := &influxRepo{
+		client: client,
+		cfg: influxdata.BatchPointsConfig{
+			Database: database,
+		},
+		batchSize: batchSize,
+		batch:     []*influxdata.Point{},
+	}
+
+	repo.tick = time.NewTicker(batchTimeout).C
+	go func() {
+		for {
+			<-repo.tick
+			repo.save()
+		}
+	}()
+
+	return repo, nil
+}
+
+func (repo *influxRepo) save() error {
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+	bp, _ := influxdata.NewBatchPoints(repo.cfg)
+	bp.AddPoints(repo.batch)
+	if err := repo.client.Write(bp); err != nil {
+		return err
+	}
+	repo.batch = []*influxdata.Point{}
+	return nil
 }
 
 func (repo *influxRepo) Save(msg mainflux.Message) error {
-	bp, err := influxdata.NewBatchPoints(influxdata.BatchPointsConfig{
-		Database: repo.database,
-	})
-	if err != nil {
-		return err
-	}
-
 	tags, fields := repo.tagsOf(&msg), repo.fieldsOf(&msg)
 	pt, err := influxdata.NewPoint(pointName, tags, fields, time.Now())
 	if err != nil {
 		return err
 	}
+	repo.add(pt)
+	return nil
+}
 
-	bp.AddPoint(pt)
-	return repo.client.Write(bp)
+func (repo *influxRepo) add(pt *influxdata.Point) {
+	repo.mu.Lock()
+	repo.batch = append(repo.batch, pt)
+	repo.mu.Unlock()
+	if len(repo.batch)%repo.batchSize == 0 {
+		repo.save()
+	}
 }
 
 func (repo *influxRepo) tagsOf(msg *mainflux.Message) tags {
--- writers/influxdb/messages_test.go
+++ writers/influxdb/messages_test.go
@@ -11,9 +11,9 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	influxdata "github.com/influxdata/influxdb/client/v2"
-	"github.com/influxdata/influxdb/models"
 	"github.com/mainflux/mainflux"
 	log "github.com/mainflux/mainflux/logger"
 	writer "github.com/mainflux/mainflux/writers/influxdb"
@@ -22,36 +22,19 @@ import (
 )
 
 var (
-	port      string
-	testLog   = log.New(os.Stdout)
-	testDB    = "test"
-	client    influxdata.Client
-	clientCfg = influxdata.HTTPConfig{
+	port          string
+	testLog       = log.New(os.Stdout)
+	testDB        = "test"
+	saveTimeout   = 5 * time.Second
+	saveBatchSize = 20
+	streamsSize   = 250
+	client        influxdata.Client
+	q             = fmt.Sprintf("SELECT * FROM test..messages\n")
+	clientCfg     = influxdata.HTTPConfig{
 		Username: "test",
 		Password: "test",
 	}
-)
-
-// This is utility function to query the database.
-func queryDB(cmd string) ([]models.Row, error) {
-	q := influxdata.Query{
-		Command:  cmd,
-		Database: testDB,
-	}
-	response, err := client.Query(q)
-	if err != nil {
-		return nil, err
-	}
-	if response.Error() != nil {
-		return nil, response.Error()
-	}
-	// There is only one query, so only one result and
-	// all data are stored in the same series.
-	return response.Results[0].Series, nil
-}
-
-func TestSave(t *testing.T) {
-	msg := mainflux.Message{
+	msg = mainflux.Message{
 		Channel:     45,
 		Publisher:   2580,
 		Protocol:    "http",
@@ -66,21 +49,70 @@ func TestSave(t *testing.T) {
 		UpdateTime:  5456565466,
 		Link:        "link",
 	}
+)
 
-	q := fmt.Sprintf("SELECT * FROM test..messages\n")
+// This is utility function to query the database.
+func queryDB(cmd string) ([][]interface{}, error) {
+	q := influxdata.Query{
+		Command:  cmd,
+		Database: testDB,
+	}
+	response, err := client.Query(q)
+	if err != nil {
+		return nil, err
+	}
+	if response.Error() != nil {
+		return nil, response.Error()
+	}
+	// There is only one query, so only one result and
+	// all data are stored in the same series.
+	return response.Results[0].Series[0].Values, nil
+}
 
+func TestSave(t *testing.T) {
 	client, err := influxdata.NewHTTPClient(clientCfg)
 	require.Nil(t, err, fmt.Sprintf("Creating new InfluxDB client expected to succeed: %s.\n", err))
 
-	repo, err := writer.New(client, testDB)
+	repo, err := writer.New(client, testDB, 1, saveTimeout)
 	require.Nil(t, err, fmt.Sprintf("Creating new InfluxDB repo expected to succeed: %s.\n", err))
 
 	err = repo.Save(msg)
 	assert.Nil(t, err, fmt.Sprintf("Save operation expected to succeed: %s.\n", err))
 
+	time.Sleep(1 * time.Second)
 	row, err := queryDB(q)
 	assert.Nil(t, err, fmt.Sprintf("Querying InfluxDB to retrieve data count expected to succeed: %s.\n", err))
 
 	count := len(row)
 	assert.Equal(t, 1, count, fmt.Sprintf("Expected to have 1 value, found %d instead.\n", count))
+}
+
+func TestBatchSave(t *testing.T) {
+	client, err := influxdata.NewHTTPClient(clientCfg)
+	require.Nil(t, err, fmt.Sprintf("Creating new InfluxDB client expected to succeed: %s.\n", err))
+
+	repo, err := writer.New(client, testDB, saveBatchSize, saveTimeout)
+	require.Nil(t, err, fmt.Sprintf("Creating new InfluxDB repo expected to succeed: %s.\n", err))
+
+	for i := 0; i < streamsSize; i++ {
+		repo.Save(msg)
+	}
+
+	row, err := queryDB(q)
+	assert.Nil(t, err, fmt.Sprintf("Querying InfluxDB to retrieve data count expected to succeed: %s.\n", err))
+	time.Sleep(time.Second)
+	count := len(row)
+	// Add one because of previous test.
+	size := streamsSize - (streamsSize % saveBatchSize) + 1
+	assert.Equal(t, size, count, fmt.Sprintf("Expected to have %d value, found %d instead.\n", size, count))
+
+	// Sleep for `saveBatchTime` to trigger timer and check if data is saved.
+	time.Sleep(saveTimeout)
+
+	row, err = queryDB(q)
+	assert.Nil(t, err, fmt.Sprintf("Querying InfluxDB to retrieve data count expected to succeed: %s.\n", err))
+	count = len(row)
+	// Add one because of previous test.
+	size = streamsSize + 1
+	assert.Equal(t, size, count, fmt.Sprintf("Expected to have %d value, found %d instead.\n", size, count))
 }
